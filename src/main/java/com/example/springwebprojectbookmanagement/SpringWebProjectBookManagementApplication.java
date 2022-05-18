1bba2c980c125f6375c3006f20ca8ed8098a7a1c
Михаил Эйсвальд <e_misha_vl@mail.ru>
2022-05-18 15:43:09 +0300

    root commit

---
+++ src/main/java/com/example/springwebprojectbookmanagement/SpringWebProjectBookManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwebprojectbookmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebProjectBookManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebProjectBookManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springwebprojectbookmanagement/SpringWebProjectBookManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwebprojectbookmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebProjectBookManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
